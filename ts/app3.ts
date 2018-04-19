e1c76d07f5d2de8187a390b3e54de3d14814d906
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2018-04-19 19:47:08 -0300

    Preparando o ambiente, exemplos ES6, exemplos TypeScript

---
+++ ts/app3.ts
@@ -0,0 +1,18 @@
+import Carro from './Carro'
+import Moto from './Moto'
+import Concessionaria from './Concessionaria'
+
+let carro = new Carro('velost', 3);
+carro.acelerar()
+carro.acelerar()
+
+let moto = new Moto();
+moto.acelerar()
+moto.acelerar()
+
+console.log(moto);
+console.log(carro)
+
+let concessionaria = new Concessionaria('', [])
+
+console.log(concessionaria.fornecerHorariosDeFuncionamento())
