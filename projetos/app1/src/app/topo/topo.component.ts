7428159fb029b03d033cc9a8ae74c6d82e0c4f59
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2018-04-20 14:41:35 -0300

    Criando o componente topo manualmente e instanciado no APP principal

---
+++ projetos/app1/src/app/topo/topo.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-topo',
+    templateUrl: './topo.component.html',
+    // styles: [`
+    //     .exemplo {
+    //         color: red;
+    //         font-size: 25px;
+    //         padding: 5px 10px;
+    //         box-shadow: 1px 1px 1px 1px #c9c9c7;
+    //         text-align: center;
+    //     }
+    // `],
+    styleUrls: ['./topo.component.css']
+})
+export class TopoComponent {}
