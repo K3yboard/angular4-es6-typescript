12def712d8846024b9f5125dd1093c9da8e4c953
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2018-04-21 13:45:48 -0300

    Adicionando lógica para verificar se a resposta está correta

--- projetos/app1/src/app/painel/painel.component.ts
+++ projetos/app1/src/app/painel/painel.component.ts
@@ -14,14 +14,31 @@ export class PainelComponent implements OnInit {
     public instrucao: string = 'Traduza a frase';
     public resposta: string;
 
-    constructor() { console.log(this.frases); }
+    public rodada: number = 0;
+    public rodadaFrase: Frase;
+
+    constructor() {
+        this.rodadaFrase = this.frases[this.rodada];
+        console.log(this.rodadaFrase);
+    }
 
     ngOnInit() {
     }
 
     public atualizaResposta(resposta: Event): void {
         this.resposta = (<HTMLInputElement>resposta.target).value;
-        console.log(this.resposta);
+        // console.log(this.resposta);
+    }
+
+    public verificarResposta(): void {
+
+        if(this.rodadaFrase.frasePtBr == this.resposta) {
+            alert('tradução correta');
+            this.rodada++;
+            this.rodadaFrase = this.frases[this.rodada];
+        } else {
+            alert('tradução errada')
+        }
     }
 
 }
