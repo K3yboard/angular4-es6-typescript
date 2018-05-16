a3307dafc8c652dec2cf017de0d37b96bc6fc0fe
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2018-05-16 20:01:37 -0300

    Exemplo de Observable

--- projetos/app2/src/app/oferta/oferta.component.ts
+++ projetos/app2/src/app/oferta/oferta.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Oferta } from '../shared/oferta.model';
 import { OfertasService } from '../ofertas.service';
 import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
 import 'rxjs/Rx';
 
 @Component({
@@ -26,16 +27,30 @@ export class OfertaComponent implements OnInit {
                 this.oferta = oferta;
             });
 
-        // this.route.params.subscribe(
-        //     (parametro: any) => { console.log(parametro); },
-        //     (erro: any) => console.log(erro),
-        //     () => console.log('Processamento foi classificado como concluído')
-        // );
+            /*
+        this.route.params.subscribe(
+            (parametro: any) => { console.log(parametro); },
+            (erro: any) => console.log(erro),
+            () => console.log('Processamento foi classificado como concluído')
+        );
 
         let tempo = Observable.interval(2000);
 
         tempo.subscribe(
             (intervalo: number) => { console.log(intervalo); }
+        );
+
+        */
+
+        // Observable (observável)
+
+        let meuObservableTeste = Observable.create((observer: Observer<number>) => {
+            observer.next(1);
+        });
+
+        // Observable (observador)
+        meuObservableTeste.subscribe(
+            (resultado: any) => console.log(resultado + 10)
         );
     }
 
