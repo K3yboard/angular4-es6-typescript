fbb38b8b8b404c3fbed19d7ebfece90405143370
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2018-05-16 19:22:25 -0300

    Exemplo de subscribe em ActivatedRoute.Params

--- projetos/app2/src/app/oferta/oferta.component.ts
+++ projetos/app2/src/app/oferta/oferta.component.ts
@@ -23,6 +23,12 @@ export class OfertaComponent implements OnInit {
             .then(( oferta: Oferta ) => {
                 this.oferta = oferta;
             });
+
+        // this.route.params.subscribe(
+        //     (parametro: any) => { console.log(parametro); },
+        //     (erro: any) => console.log(erro),
+        //     () => console.log('Processamento foi classificado como concluído')
+        // );
     }
 
 }
