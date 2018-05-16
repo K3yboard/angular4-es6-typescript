b295fb0cf9f3468a41b975816e4ff2b301743745
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2018-05-16 19:33:12 -0300

    Observables - exemplo de interval operator

--- projetos/app2/src/app/oferta/oferta.component.ts
+++ projetos/app2/src/app/oferta/oferta.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Oferta } from '../shared/oferta.model';
 import { OfertasService } from '../ofertas.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 @Component({
     selector: 'app-oferta',
@@ -29,6 +31,12 @@ export class OfertaComponent implements OnInit {
         //     (erro: any) => console.log(erro),
         //     () => console.log('Processamento foi classificado como concluído')
         // );
+
+        let tempo = Observable.interval(2000);
+
+        tempo.subscribe(
+            (intervalo: number) => { console.log(intervalo); }
+        );
     }
 
 }
