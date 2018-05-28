086548dd5de1d10a39404894d3d002892c2c6379
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2018-05-28 11:52:42 -0300

    Adicionando componente list-group para listar itens da pesquisa

--- projetos/app2/src/app/topo/topo.component.ts
+++ projetos/app2/src/app/topo/topo.component.ts
@@ -20,15 +20,17 @@ export class TopoComponent implements OnInit {
 
     public ofertas: Observable<Oferta[]>;
     private subjectPesquisa: Subject<string> = new Subject<string>();
+    public ofertas2: Oferta[];
 
     constructor(private ofertasService: OfertasService) { }
 
     ngOnInit() {
-        this.ofertas = this.subjectPesquisa
-            .debounceTime(1000)
-            .distinctUntilChanged()
+        this.ofertas = this.subjectPesquisa // retorna Oferta[]
+            .debounceTime(1000) // executa a ação do switchMap após 1 segundo
+            .distinctUntilChanged() // oara fazer pesquisas distintas
             .switchMap((termo: string) => {
                 if (termo.trim() === '') {
+                    // retorna um observable de array de ofertas vazio
                     return Observable.of<Oferta[]>([]);
                 }
                 console.log('requisição http para api');
@@ -39,7 +41,10 @@ export class TopoComponent implements OnInit {
                 return Observable.of<Oferta[]>([]);
             });
 
-        this.ofertas.subscribe((ofertas: Oferta[]) => console.log(ofertas));
+        this.ofertas.subscribe((ofertas: Oferta[]) => {
+            console.log(ofertas);
+            this.ofertas2 = ofertas;
+        });
     }
 
     public pesquisa(termoDaBusca: string): void {
